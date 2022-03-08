a2e4dc6282f52977d020647e14b1761239acc12d
SamLiu <amazing_sam@outlook.com>
2022-03-08 20:26:52 +0800

    feat: ✅不用模块，不用命名空间的简单版本

--- with-namespace/src/store/index.js
+++ with-namespace/src/store/index.js
@@ -16,29 +16,35 @@ export default new Vuex.Store({
     }
   },
   getters: {
+    // 两倍count
     doubleCount(state) {
       return state.count * 2
     },
+    // 四倍count
     fourfoldCount(state, getters) {
-      console.log(getters, 2)
       return getters.doubleCount * 2
     }
   },
   mutations: {
+    // 重置count
     resetCount(state) {
       state.count = 0
     },
+    // 增加count
     addCount(state, payload) {
       state.count += payload
     },
+    // 设置count mutation
     setCountMutation(state, payload) {
-      console.log(payload, 2929)
-      state.count = payload
+      console.log(payload)
+      state.count = payload.value
     }
   },
   actions: {
-    setCountAction({commit}, payload) {
-      commit('setCountMutation', payload)
+    // 设置count action
+    setCountAction(ctx, payload) {
+      console.log(payload)
+      ctx.commit('setCountMutation', payload)
     }
   }
 })
