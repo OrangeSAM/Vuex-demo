b9b3502100c64eab5fe5d9b29d5afce20510cce5
SamLiu <amazing_sam@outlook.com>
2022-03-09 22:07:15 +0800

    feat: ✅区分模块未开命名空间的vuex demo

---
+++ 2.0/src/components/moduleA.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="moduleA">
+    <div class="content">
+      <div>模块A的count: {{count}}</div>
+      <div>模块A的双倍count: {{doubleModuleACount}}</div>
+    </div>
+    <div class="operate">
+      <button @click="addMutation">增加 count</button>
+      <button @click="submitMutation">提交 mutation</button>
+      <button @click="submitAction">提交 action</button>
+      <button @click="submitReset">重置 count</button>
+    </div>
+    <div class="operate">
+      <button @click="submitMutation1">提交顶层 store 中的 mutation</button>
+      <button @click="submitAction1">提交顶层 store 中的 action</button>
+    </div>
+    <div class="operate">
+      <button @click="addModuleBCount">提交模块B store 中的 mutation</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapActions, mapGetters, mapMutations, mapState} from "vuex";
+
+export default {
+  name: "moduleA",
+  computed: {
+    ...mapState({
+      count: state => state.moduleA.moduleACount
+    }),
+    ...mapGetters(['doubleModuleACount'])
+  },
+  methods: {
+    ...mapMutations(['addModuleACountMutation', 'setModuleACountMutation',
+      'resetModuleACount', 'addModuleBCountMutation',
+      'addCount']),
+    ...mapActions(['setModuleACountAction', 'setCountAction']),
+    // 增加模块count的值
+    addMutation() {
+      this.addModuleACountMutation(10)
+    },
+    // 提交模块A的mutation
+    submitMutation() {
+      this.setModuleACountMutation('这是一个模块A的mutation')
+    },
+    // 提交模块A的action
+    submitAction() {
+      this.setModuleACountAction('这是一个模块A的action')
+    },
+    // 重置模块A的count
+    submitReset() {
+      this.resetModuleACount()
+    },
+    // 提交顶层store的mutation
+    submitMutation1() {
+      this.addCount(20)
+      console.log(this.$store)
+    },
+    // 提交顶层store的action
+    submitAction1() {
+      this.setCountAction({value: '模块A设置顶层store的count'})
+    },
+    // 修改模块B的count
+    addModuleBCount() {
+      this.addModuleBCountMutation(15)
+    },
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ 2.0/src/components/moduleB.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="moduleB">
+    <div class="content">
+      <div>模块B的count: {{count}}</div>
+    </div>
+    <div class="operate">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapState} from "vuex";
+
+export default {
+  name: "moduleB",
+  computed: {
+    ...mapState({
+      count: state => state.moduleB.moduleBCount
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
